a46ecc9369d235bab6808ebc8f1c51b1661fba8f
Charles <eganmc@outlook.com>
2025-07-11 16:26:18 +0100

    added Heroku integration and Procfile, solved functional homepage

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d2=%+$=xuvqm7ad36m_)8g*uu!klec+$j*^^-pvwdoj^fm!7qo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
